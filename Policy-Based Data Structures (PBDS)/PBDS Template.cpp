458cea1a61979fb055390327fb8630ccdea48c11
Md. Sishir Rahman Siam <162951877+shishirRsiam@users.noreply.github.com>
2024-12-13 19:01:46 +0600

    Update PBDS Template.cpp

--- Policy-Based Data Structures (PBDS)/PBDS Template.cpp
+++ Policy-Based Data Structures (PBDS)/PBDS Template.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 
-using namespace std;
-
 template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 
 int main() 
 {
